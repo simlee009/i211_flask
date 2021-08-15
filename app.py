db4b938c5dcfb8a94da87d126dbedac9127971fe
Simon Lee <simlee009@gmail.com>
2021-08-15 14:32:46 -0400

    Updated to show list of events in index template
    Updated app.py and index.html to render the list of events given in
    events.csv. The rendering is pretty basic at the moment and needs some
    more tweaking.

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
+import csv
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    events = read_csv('events.csv')
+    return render_template('index.html', events=events)
+
+def read_csv(file_name):
+    """Helper function to read in CSV data as a list of dictionaries."""
+    with open(file_name) as csv_file:
+        reader = csv.DictReader(csv_file)
+        results = list(reader)
+    return results
 
 if __name__ == '__main__':
     app.run(debug = True)
