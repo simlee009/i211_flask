2455e5e78ff480d5027cbe9e479f961da738607f
Simon Lee <simlee009@gmail.com>
2021-08-15 22:40:21 -0400

    Added ability to show individual events
    Updated the events list to link to an individual event by ID.
    
    Also made some tweaks to app.py, including setting more docstrings and
    improrving the "events" route.

--- app.py
+++ app.py
@@ -5,10 +5,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """Show the home page."""
     return render_template('index.html')
 
-@app.route('/events')
-def events():
+@app.route('/events/')  # Using a trailing slash allows Flask to handle both "/event" and "/event/".
+@app.route('/events/<id>')
+def events(id=None):
+    """Show an event if an ID is specified. Otherwise list all events."""
+    if id:
+        # Once we get an id, we need to try to match it to an event in our list.
+        # Instead of using some fancy Python, just do a simple loop.
+        for event in events:
+            if event["id"] == id:
+                # Found the id!
+                return render_template('event.html', event=event)
+        print(f"No event found with id of {id}.")
+    # The default is to render the events list.
     return render_template('events.html', events=events)
 
 def read_csv(file_name):
