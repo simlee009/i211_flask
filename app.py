494bd19164337610e41874fada77b4e16d54f5bd
Simon Lee <simlee009@gmail.com>
2021-08-15 21:34:42 -0400

    Refactored events listing
    Moved the events list into its own route. The index is now just a
    landing page.

--- app.py
+++ app.py
@@ -5,8 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    events = read_csv('events.csv')
-    return render_template('index.html', events=events)
+    return render_template('index.html')
+
+@app.route('/events')
+def events():
+    return render_template('events.html', events=events)
 
 def read_csv(file_name):
     """Helper function to read in CSV data as a list of dictionaries."""
@@ -14,6 +17,10 @@ def read_csv(file_name):
         reader = csv.DictReader(csv_file)
         results = list(reader)
     return results
+
+# Read in the events at app start and keep it in memory, instead of reading it
+#  every single time the events page loads.
+events = read_csv('events.csv')
 
 if __name__ == '__main__':
     app.run(debug = True)
