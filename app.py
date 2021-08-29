1c2355e6dc439c189d265335b36337a2e6ba542f
Simon Lee <simlee009@gmail.com>
2021-08-29 14:57:21 -0400

    Added route for viewing a specific event

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 import csv  # Used for reading and writing event data via csv.
 
@@ -12,6 +12,15 @@ def index():
 def events():
     events = get_events()
     return render_template('events.html', events=events)
+
+@app.route('/events/<event_id>/')
+def event(event_id=None):
+    if event_id:
+        event_id = int(event_id)
+        events = get_events()
+        return render_template('event.html', event=events[event_id])
+    else:
+        return redirect(url_for('events'))
 
 def get_events():
     file_path = app.root_path + '/events.csv'
