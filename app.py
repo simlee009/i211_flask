50033e044dfff642b6209ab2609eea2708c83238
Simon Lee <simlee009@gmail.com>
2021-08-29 14:18:46 -0400

    Added events list
    Created a route and template for rendering the list of events. Also
    added a simple function to read the events data in from a CSV. Then
    created a link to the events from the index page.

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
 from flask import Flask, render_template
 
+import csv  # Used for reading and writing event data via csv.
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/events/')
+def events():
+    events = get_events()
+    return render_template('events.html', events=events)
+
+def get_events():
+    file_path = app.root_path + '/events.csv'
+    results = []
+    try:
+        with open(file_path) as csv_file:
+            reader = csv.DictReader(csv_file)
+            results = list(reader)
+    except Exception as err:
+        print(err)
+    return results
 
 if __name__ == '__main__':
     app.run(debug = True)
